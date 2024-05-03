50cad54039b76c952ce6760170726f8c2c3f41e9
di93fuj <ajaynr1995@gmail.com>
2024-05-03 18:31:20 +0200

    first commit

---
+++ lightning/vgg-resnet/vgg-resnet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+##SBATCH --partition=lrz-hpe-p100x4,lrz-dgx-1-p100x8,lrz-dgx-1-v100x8,lrz-dgx-a100-80x8
+#SBATCH -p test-v100x2
+#SBATCH -q testing
+#SBATCH --gres=gpu:1
+#SBATCH -o vgg-resnet-%J.out
+#SBATCH -t 02:00:00
+
+srun \
+--container-mounts='../../:/workspace' \
+--container-image='/dss/dsshome1/0D/di93fuj/dssContainer/ajay/containerImages/custom.sqsh' \
+python /workspace/lightning/vgg-resnet/vgg-resnet.py
