3860cc092a35a0316a13c8797da5dc333f5a7be2
di93fuj <ajaynr1995@gmail.com>
2024-05-05 23:25:45 +0200

    Generalized the jobfiles

--- torch/horovod/horovod-exercise.sh
+++ torch/horovod/horovod-exercise.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=2
-##SBATCH -p lrz-v100x2,lrz-hpe-p100x4,lrz-dgx-1-p100x8,lrz-dgx-1-v100x8,lrz-dgx-a100-80x8
-#SBATCH -p test-v100x2 --qos=testing
+#SBATCH --partition=ADD_PARTITION_HERE
 #SBATCH --gres=gpu:2
 #SBATCH -o horovod-exercise.out%J
 #SBATCH -e horovod-exercise.err%J
 #SBATCH --container-mounts='../../:/workspace'
-#SBATCH --container-image='/dss/dsshome1/0D/di93fuj/containerImages/flash.sqsh'
+#SBATCH --container-image='path/to/your/image/image_name.sqsh'
 
 horovodrun -np 2 python /workspace/torch/horovod/horovod-exercise.py
