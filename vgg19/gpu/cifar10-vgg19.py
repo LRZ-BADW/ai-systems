bf8e33c869ae2654a9d4f4ddc4994bdb4a36cae1
di93fuj <ajaynr1995@gmail.com>
2024-11-05 15:39:54 +0100

    Removed the number of test images

--- vgg19/gpu/cifar10-vgg19.py
+++ vgg19/gpu/cifar10-vgg19.py
@@ -105,7 +105,7 @@ def main(args):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+    print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
 
 if __name__ == '__main__':
     
